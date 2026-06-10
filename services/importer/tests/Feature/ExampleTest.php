ab50e4b97ad7d219402a06cd1ee107b2ccfa07a1
Oscar Guevara <yhonguevara@gmail.com>
2026-06-10 21:01:09 +0000

    fix(test): update ExampleTest to expect redirect on index

--- services/importer/tests/Feature/ExampleTest.php
+++ services/importer/tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertRedirect('/upload');
+        $response->assertStatus(302);
     }
 }
